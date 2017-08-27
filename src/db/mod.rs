e8f66c7440a42d3f0c60cc573f8cf5c0cc66431e
Adam Childs <adamdchilds@gmail.com>
2017-08-26 22:27:03 -0400

    Remove unused code

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -7,9 +7,6 @@ const DB_NAME: &'static str = "db.tav";
 
 pub const SQL_MD5_SELECT: &'static str = "SELECT name FROM malware WHERE md5 = ?";
 
-///
-///
-///
 pub struct Database {
 
     pub location: PathBuf,
@@ -74,47 +71,4 @@ fn get_default_location() -> PathBuf {
     default_location.push_str(DB_NAME);
 
     PathBuf::from(default_location)
-}
-
-#[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    time_created: Timespec,
-    data: Option<Vec<u8>>
-}
-
-pub fn whatever() {
-    let conn: Connection = Connection::open("C:\\Users\\adam8\\Desktop\\data.db").unwrap();
-
-    conn.execute("CREATE TABLE IF NOT EXISTS person (
-                  id              INTEGER PRIMARY KEY,
-                  name            TEXT NOT NULL,
-                  time_created    TEXT NOT NULL,
-                  data            BLOB
-                  )", &[]).unwrap();
-
-    let me: Person = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        time_created: time::get_time(),
-        data: None
-    };
-    conn.execute("INSERT INTO person (name, time_created, data)
-                  VALUES (?1, ?2, ?3)",
-                 &[&me.name, &me.time_created, &me.data]).unwrap();
-
-    let mut stmt = conn.prepare("SELECT id, name, time_created, data FROM person").unwrap();
-    let person_iter = stmt.query_map(&[], |row| {
-        Person {
-            id: row.get(0),
-            name: row.get(1),
-            time_created: row.get(2),
-            data: row.get(3)
-        }
-    }).unwrap();
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
 }
